d315d5483e88056c10556db16ca7641b6458b6d9
miami <bapolygalov@edu.hse.ru>
2022-10-09 18:45:31 +0500

    Создал проект, добавил в Main.storyboard label и button, и реализовал работу кнопки

--- YP-HW1/ViewController.swift
+++ YP-HW1/ViewController.swift
@@ -9,11 +9,18 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var countLabel: UILabel!
+    @IBOutlet weak var incCountButton: UIButton!
+    var count: Int = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func incCount() {
+        count += 1
+        countLabel.text = "Значение счётчика: \(count)"
+    }
 }
 
