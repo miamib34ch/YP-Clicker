2e60dca66864c61cdc890e24598eee5875d164ff
miami <bapolygalov@edu.hse.ru>
2022-10-09 18:31:01 +0500

    Initial Commit

---
+++ YP-HW1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  YP-HW1
+//
+//  Created by Богдан Полыгалов on 09.10.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
